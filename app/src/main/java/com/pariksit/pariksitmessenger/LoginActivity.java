e12828db71083aad31b6665964b4375e2d2c50ca
Yogesh Prajapati <yogeshbndm@gmail.com>
2020-10-25 20:21:39 +0530

    stage1

--- app/src/main/java/com/pariksit/pariksitmessenger/LoginActivity.java
+++ app/src/main/java/com/pariksit/pariksitmessenger/LoginActivity.java
@@ -18,7 +18,7 @@ import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.androidnetworking.interfaces.ParsedRequestListener;
 import com.google.gson.Gson;
-import com.pariksit.pariksitmessenger.ChatFragment.User;
+import com.pariksit.pariksitmessenger.ChatFragment.MainUser;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -69,9 +69,9 @@ public class LoginActivity extends AppCompatActivity {
                 .setTag("test")
                 .setPriority(Priority.MEDIUM)
                 .build()
-                .getAsObject(User.class, new ParsedRequestListener<User>() {
+                .getAsObject(MainUser.class, new ParsedRequestListener<MainUser>() {
                     @Override
-                    public void onResponse(User user) {
+                    public void onResponse(MainUser user) {
                         // do anything with response
                         if("".equals(user.getId())) {
                             //uid null, means login failed
@@ -89,13 +89,14 @@ public class LoginActivity extends AppCompatActivity {
                 });
     }
 
-    private void saveDataInPrefs(User user) {
+    private void saveDataInPrefs(MainUser user) {
         prefsEditor.putString("mainUser", user.toString());
         prefsEditor.commit();
         moveToMainPage();
     }
 
     private void moveToMainPage() {
+        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(LoginActivity.this , MainActivity.class);
         startActivity(intent);
     }
